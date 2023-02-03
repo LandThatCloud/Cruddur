10b638a00fa93d1ae2a157ebe17058afad561e54
Ram Ramaiyah <ramramaiyah@Rams-Laptop.local>
2023-02-03 13:23:20 -0500

    Cruddur

---
+++ app/app.rb
@@ -0,0 +1,10 @@
+require 'sinatra'
+require "sinatra/namespace"
+require 'json'
+
+namespace '/api' do
+  get '/activities' do
+    content_type 'application/json'
+    { hello: "World" }.to_json
+  end
+end
